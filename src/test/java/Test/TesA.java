dafbc9bff5ecbffa7484aa3cb2251e483c9b3ad4
maguoqing0109 <389765150@qq.com>
2020-11-27 23:06:53 +0800

    add tb

---
+++ src/test/java/Test/TesA.java
@@ -0,0 +1,31 @@
+package Test;
+
+import SeleniumFrameworkMethod.ReadLog4j;
+import WebDrivers.BrowserTypes;
+import WebDrivers.Drivers;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TesA {
+    private final ReadLog4j log = new ReadLog4j( TesA.class );
+    private WebDriver driver;
+
+    @BeforeClass
+    public void methodA() {
+        Drivers drivers = new Drivers( BrowserTypes.chrome );
+        driver = drivers.driver;
+    }
+
+    @Test
+    public void methodB() {
+        log.info( "打开淘宝网页" );
+        driver.get( "http://www.taobao.com/" );
+    }
+
+    @AfterMethod
+    public void methodC() {
+        driver.quit();
+    }
+}
